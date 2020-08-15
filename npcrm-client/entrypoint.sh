434ecd968e753a23877ec659cba250d6a1f8eb20
Henry Brown <h@0ti.me>
2020-08-15 11:16:17 -0500

    Fix entrypoint.sh

--- npcrm-client/entrypoint.sh
+++ npcrm-client/entrypoint.sh
@@ -5,7 +5,7 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 main() {
   /nvm.sh
 
-  yarn global add @vue/cli @vue/cli-service-global
+  /nvm.sh yarn global add @vue/cli @vue/cli-service-global
 
   /nvm.sh "$@"
 }
